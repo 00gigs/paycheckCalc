0c91623a8b5a86a9504a9c91e3847ee22b594812
00gigs <1onlinegigs@gmail.com>
2024-02-29 16:49:37 -0500

    resposive UI sm: screen fix login

--- app/components/Signin.js
+++ app/components/Signin.js
@@ -48,10 +48,10 @@ const router = useRouter()
   return (
     <div>
       <div className="flex justify-center h-screen items-center ">
-        <div className="border-spacing-2 m-3 border-4 p-4 border-orange-300 md:h-80 md:w-full ">
+        <div className="border-spacing-2 m-3 border-4 p-4 border-orange-300 md:h-80 md:w-full">
           <form
             name="form"
-            className="flex flex-col gap-5 md:text-2xl"
+            className="flex flex-col gap-5 md:text-2xl sm:text-sm w-40"
             method="post"
             onSubmit={handleSubmit}
           >
